af525325bdc63a9c25029e116ed328f270ceeb08
Valeriya Ovseychuk <v.ovseychuk@appkode.ru>
2022-06-19 13:57:03 +0300

    Added lib file

---
+++ SwiftyLib/SwiftyLib.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftyLib.swift
+//  SwiftyLib
+//
+//  Created by Валерия Овсейчук on 19.06.22.
+//
+
+public final class SwiftyLib {
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+}
